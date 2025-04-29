4a0ad885b0ca522d9e55aa945f651948d4330251
kyrios12 <saahilhumayun@gmail.com>
2025-04-30 00:07:58 +0530

    Intial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+// import './App.css'
+
+import CohortGraph from "./Components/cohort"
+import LayerCake from "./Components/layercake"
+
+function App() {
+
+  return (
+    <>
+    <CohortGraph/>
+    <LayerCake/>
+    </>
+  )
+}
+
+export default App
